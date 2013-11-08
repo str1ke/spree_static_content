a8d23e9fb243ed2918e3d47d82df25e9ad982a0a
Andrey Sokolov <a.sokolov@speople.pro>
2013-11-08 12:05:25 +0700

    hardcode html to solve route exception in tab() helper

--- app/overrides/static_content_admin_tab.rb
+++ app/overrides/static_content_admin_tab.rb
@@ -1,5 +1,13 @@
+html = <<HTML
+<li class="tab-with-icon">
+  <a class="icon_link with-tip icon-user" href="/admin/pages">
+    <span class="text">Pages</span>
+  </a>
+</li>
+HTML
+
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "static_content_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:pages, :icon => 'icon-file') %>",
+                     :text => html,
                      :disabled => false)
